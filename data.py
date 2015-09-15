66f1b15868698ab5d2c510a2205ad41d942ed1c2
Lukas Michanek <michanek.lukas@gmail.com>
2015-09-15 10:30:16 +0200

    Added search functionality

--- data.py
+++ data.py
@@ -23,8 +23,37 @@ def get_project(db, id):
             return project
     return None
 
+def has_techniques(project, techniques):
+    if techniques == None: return True
+    project_techniques = set(project['techniques_used'])
+    return set(techniques) <= project_techniques
+
+def search_parameter_in_field(project, fields, search_parameter):
+    if search_parameter == None: return True
+    for field in fields:
+        field_value = project[field]
+        if isinstance(field_value,str):
+            if search_parameter.lower() in field_value.lower(): return True
+        elif isinstance(field_value,int):
+            try:
+                search_number = int(search_parameter)
+                if search_number == field_value: return True
+            except:
+                pass
+    return False
+
 def search(db, sort_by = 'start_date', sort_order = 'desc', techniques = None, search = None, search_fields = 0):
-    return []
+    filtered_projects = []
+
+    for project in db:
+        pprint.pprint(project)
+        print(has_techniques(project, techniques) and search_parameter_in_field(project, search_fields, search))
+        if has_techniques(project, techniques) and search_parameter_in_field(project, search_fields, search):
+            filtered_projects.append(project)
+
+    filtered_projects = sorted(filtered_projects,key=lambda x: x[sort_by], reverse=sort_order=='desc')
+
+    return filtered_projects
 
 def get_techniques(db):
     techniques = []
