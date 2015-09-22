80f75304b8e3ad9e8a9350858710e65775bf124d
adaiv505 <adaiv505@liu.se>
2015-09-22 23:14:00 +0200

    Added basic functionality to the /list part of the portfolio

--- presentation.py
+++ presentation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
+import data
 from flask import Flask
 from flask import render_template
 
@@ -7,9 +8,18 @@ from flask import render_template
 app = Flask(__name__)
 
 
+
 @app.route('/')
-def hello_world():
-    return render_template('main.html', heading = "Projektnamn", long_text = "Lorem ipsum text text text osv osv you get the point")
+def main_page():
+	db = data.load("data.json")
+	example_project = data.search(db)[0]
+	return render_template('main.html', project_data = example_project)
+
+@app.route('/list')
+def list_page():
+	db = data.load("data.json")
+	result_list = data.search(db)
+	return render_template('list.html', project_list = result_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
